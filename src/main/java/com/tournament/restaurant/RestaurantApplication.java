0862849464c59a3fe664d7cd2976edfad64f24ee
taurus366 <47596673+taurus366@users.noreply.github.com>
2021-05-18 15:57:19 +0300

    test

--- src/main/java/com/tournament/restaurant/RestaurantApplication.java
+++ src/main/java/com/tournament/restaurant/RestaurantApplication.java
@@ -2,12 +2,21 @@ package com.tournament.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class RestaurantApplication {
+@EnableSwagger2
+public class RestaurantApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(RestaurantApplication.class);
+    }
 }
